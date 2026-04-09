fd1d27d64b43abfd089c13ada4809f2b6cf39745
Huncho's Macbook Pro <macbookpro@Hunchos-MacBook-Pro-16-inch.local>
2026-04-09 12:46:03 -0400

    updated advanced query

---
+++ advanced_query/1_top_paying_jobs.sql
@@ -0,0 +1,16 @@
+SELECT  
+    job_id,
+    company_dim.name AS company_name,
+    job_title,
+    salary_year_avg,
+    job_location,
+    job_schedule_type,
+    job_posted_date
+FROM job_postings_fact jpc
+LEFT JOIN company_dim ON jpc.company_id = company_dim.company_id
+WHERE job_location = 'Anywhere' AND
+    (job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL)
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
